849b07f0951cf75fa7bd376b157edadb9e46f6be
Nouzan <clobraindie@outlook.com>
2023-09-28 17:28:26 +0800

    context: add `Cache` layer and `layer_fn`

---
+++ src/context/layer/cache.rs
@@ -0,0 +1,112 @@
+use core::ops::Deref;
+
+use crate::{
+    context::{Context, ContextOperator, IntoValue, Value},
+    Operator,
+};
+
+use super::Layer;
+
+/// Layer that *caches* and *clears* the final context,
+/// and then provides it to the next evaluation.
+#[derive(Debug, Clone, Copy, Default)]
+pub struct Cache;
+
+impl<T, P> Layer<T, P> for Cache
+where
+    P: ContextOperator<T>,
+{
+    type Output = CacheOperator<P>;
+
+    fn layer(&self, inner: P) -> Self::Output {
+        CacheOperator {
+            inner,
+            previous: Previous::default(),
+        }
+    }
+}
+
+/// Wrapper for previous context.
+#[derive(Debug, Default)]
+pub struct Previous(Context);
+
+impl Previous {
+    fn take(&mut self) -> Self {
+        core::mem::take(self)
+    }
+}
+
+impl Deref for Previous {
+    type Target = Context;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+/// Operator for `Cache`.
+#[derive(Debug)]
+pub struct CacheOperator<P> {
+    inner: P,
+    previous: Previous,
+}
+
+impl<T, P> Operator<Value<T>> for CacheOperator<P>
+where
+    P: ContextOperator<T>,
+{
+    type Output = Value<<P::Out as IntoValue>::Inner>;
+
+    fn next(&mut self, mut input: Value<T>) -> Self::Output {
+        input.context_mut().insert(self.previous.take());
+        let mut output = self.inner.call(input).into_value();
+        self.previous
+            .0
+            .extend(core::mem::take(output.context_mut()));
+        output
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{
+        context::{input, layer::layer_fn},
+        IndicatorIteratorExt,
+    };
+
+    use super::*;
+
+    #[test]
+    fn squre_cache() {
+        struct Square<P>(P);
+
+        impl<P> Operator<Value<i32>> for Square<P>
+        where
+            P: ContextOperator<i32>,
+        {
+            type Output = P::Out;
+
+            fn next(&mut self, mut input: Value<i32>) -> Self::Output {
+                input.apply(|v, ctx| {
+                    let prev = ctx
+                        .get::<Previous>()
+                        .and_then(|prev| prev.get::<i32>().copied())
+                        .unwrap_or(0);
+                    println!("prev: {}", prev);
+                    ctx.insert(prev.pow(2) + *v);
+                });
+                self.0.call(input)
+            }
+        }
+
+        let op = input().with(layer_fn(|op| Square(op))).with(Cache);
+
+        let data = [1, 2, 3, 4, 5];
+        data.into_iter()
+            .map(Value::new)
+            .indicator(op)
+            .for_each(|v| {
+                println!("input: {}", v.into_inner());
+            });
+    }
+}
--- src/context/layer/mod.rs
+++ src/context/layer/mod.rs
@@ -1,5 +1,9 @@
 use super::ContextOperator;
 
+/// Layer that caches the final context,
+/// and provides it to the next evaluation.
+pub mod cache;
+
 /// Layer.
 /// Convert an `In`-operator to another `In`-operator.
 pub trait Layer<T, P>
@@ -11,4 +15,27 @@ where
 
     /// Convert an `In`-operator to another `In`-operator.
     fn layer(&self, operator: P) -> Self::Output;
+}
+
+/// Layer defined by a closure.
+#[derive(Debug, Clone, Copy)]
+pub struct LayerFn<F>(F);
+
+impl<F, T, P, P2> Layer<T, P> for LayerFn<F>
+where
+    F: Fn(P) -> P2,
+    P: ContextOperator<T>,
+    P2: ContextOperator<T>,
+{
+    type Output = P2;
+
+    #[inline]
+    fn layer(&self, operator: P) -> Self::Output {
+        (self.0)(operator)
+    }
+}
+
+/// Create a layer from a closure.
+pub fn layer_fn<F>(f: F) -> LayerFn<F> {
+    LayerFn(f)
 }
